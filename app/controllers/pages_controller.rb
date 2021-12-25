73e9c824be002d97e3e9c67bd8c7565d251893c8
Alastair Grant <alastairegrant@gmail.com>
2021-12-25 23:01:23 +0000

    bugs fixed, problem added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,6 @@ class PagesController < ApplicationController
   end
 
   def home
-    @problems = ["I'm contemplating suicide.", "I'm too intoxicated.", "I think I'm in danger.", "My friend is in danger."]
+    @problems = ["I'm contemplating suicide.", "I'm too intoxicated.", "I think I'm in danger.", "My friend is in danger.", "I'm being abused"]
   end
 end
